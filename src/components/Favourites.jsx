443016e21f927fe7f435764cc28fb41bb7323ffd
Esosa <esosaisnow@gmail.com>
2024-09-05 10:30:01 +0100

    fixed favoruites single block bug

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -1,11 +1,7 @@
 import React from "react";
-
+import "./Favourites.css";
 
 function Favourites({ favourites, onRemoveFromFavourites }) {
-  React.useEffect(() => {
-    console.log("The Favourites Page has mounted");
-  }, []);
-
   return (
     <div className="container mt-5">
       <h2 className="title is-4">Favourites</h2>
@@ -13,13 +9,14 @@ function Favourites({ favourites, onRemoveFromFavourites }) {
       {favourites.length === 0 ? (
         <p>No favourites added yet.</p>
       ) : (
-        <div className="columns is-multiline">
-          {favourites.map((brewery) => (
-            <div className="column is-one-third" key={brewery.id}>
-              <div className="card">
-                <div className="card-content">
-                  <div className="content">
-                    <h3 className="title is-5 brewery-name">{brewery.name}</h3>
+        // Add a new wrapper div with a class for centering
+        <div className="favourites-wrapper">
+          <div className="columns is-multiline">
+            {favourites.map((brewery) => (
+              <div className="column is-one-third-widescreen is-half-desktop is-full-tablet" key={brewery.id}>
+                <div className="card favourite-card">
+                  <div className="card-content">
+                    <h3 className="title is-5">{brewery.name}</h3>
                     <p><strong>Brewery Type:</strong> {brewery.brewery_type}</p>
                     <p><strong>City:</strong> {brewery.city}</p>
                     <p><strong>Country:</strong> {brewery.country}</p>
@@ -34,8 +31,8 @@ function Favourites({ favourites, onRemoveFromFavourites }) {
                   </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
       )}
     </div>
